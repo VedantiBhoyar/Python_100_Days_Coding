d819e18b86e93fba19b67c2b8f3d20a5e6a747be
Vedanti Bhoyar <bhoyar.veda@gmail.com>
2025-08-29 00:24:32 +0530

    Create Day2.py

---
+++ Day2.py
@@ -0,0 +1,12 @@
+// https://www.hackerrank.com/challenges/text-wrap/problem?isFullScreen=true
+
+import textwrap
+
+def wrap(string, max_width):
+    modified_string = textwrap.wrap(string,max_width)
+    return "\n".join(modified_string)
+
+if __name__ == '__main__':
+    string, max_width = input(), int(input())
+    result = wrap(string, max_width)
+    print(result)
